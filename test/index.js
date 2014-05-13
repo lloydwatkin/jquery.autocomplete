69a0a0382b4439a18470cc41296ce9d18cfb0c21
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-05-13 22:59:17 +0100

    Initial simple test

--- test/index.js
+++ test/index.js
@@ -1,21 +1,58 @@
-var page = require('webpage').create()
-var complete = false
+var Webdriver = require('selenium-webdriver')
+require('webdriverjs-helper')
+require('should')
 
-page.onConsoleMessage = function (msg) {
-    console.log(msg)
-    if (-1 !== msg.indexOf('RESULT')) {
-        var exit = 0
-        if (-1 !== msg.indexOf('not ok')) {
-            exit = 1
-        }
-        phantom.exit(exit)  
+var getBrowser = function(done) {
+    var browserToUse = process.env.BROWSER || 'phantomjs'
+    var browser, capabilities
+    switch (browserToUse) {
+        case 'phantomjs':
+        default:
+            capabilities = Webdriver.Capabilities.phantomjs()
+            break
+        case 'firefox':
+            capabilities = Webdriver.Capabilities.firefox()
+            break
+        case 'chrome-remote':
+            capabilities = Webdriver.Capabilities.chrome()
+            break
+        case 'chrome':
+            var browser = new Webdriver.Builder()
+                .withCapabilities(Webdriver.Capabilities.chrome())
+                .build()
+            done(browser)
+            return
     }
+
+    SeleniumServer = require('selenium-webdriver/remote').SeleniumServer
+    var server = new SeleniumServer(
+        'test/resources/selenium-server-standalone-2.39.0.jar',
+        { port: 4444 }
+    )
+    server.start().then(function() {
+
+        var browser = new Webdriver.Builder()
+            .usingServer(server.address())
+            .withCapabilities(capabilities)
+            .build()
+        done(browser)
+    })
 }
 
-page.open('test/test.html', function (status) {
-    
-    if (status !== 'success') {
-        console.log('FAIL to load test file')
-        phantom.exit(1)
-    }
+getBrowser(function(driver) {
+  driver.get(process.cwd() + '/test/test.html')
+  driver.element('#test1').sendKeys('M')
+  driver.elements('div.autocomplete div').count(function(length) {
+    length.should.equal(0)
+  })
+  driver.element('#test1').sendKeys('a')
+  driver.elements('div.autocomplete div').count(function(length) {
+    length.should.equal(2)
+  })
+  driver.element('div.autocomplete div').html(function(html) {
+    html.should.equal('<strong>Ma</strong>rch')
+  })
+
+
+  driver.quit()
 })
